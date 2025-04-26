b6b45bc9915358c8061523be88409721cc7608f5
om2726 <oomprkash244@gmail.com>
2025-04-26 11:45:44 +0530

    Add files via upload

---
+++ Animal.java
@@ -0,0 +1,16 @@
+public class Animal{
+public static void main(String args[]);{
+displayinfo();
+System.out.println("I am a Animal....!");
+}
+public class tiger extends Animal{
+public static void main(String args[]);{
+displayinfo();
+System.out.println("I am a tiger....!");
+}
+public class tiger{
+public static void main(String args[]);{
+Tiger tr1=new tiger();
+tr1=displayinfo();
+}
+}
